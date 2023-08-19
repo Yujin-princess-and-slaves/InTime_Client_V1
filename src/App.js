3585ef0310471f3b2578efc014f58665614dd3d6
E-jiwoo <livei07@naver.com>
2023-08-20 04:19:52 +0900

    FEAT :: 회색 상자 추가 - 왼쪽 회색 상자를 추가하였다

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
-import './App.css';
+import styled, { createGlobalStyle } from "styled-components";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-     
-    </div>
+    <>
+      <GlobalStyle />
+      <Wrapper>
+        <Square />
+      </Wrapper>
+    </>
   );
-}
+};
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+`;
+
+const Square = styled.div`
+  background-color: #929bb8;
+  width: 516px;
+  height: 100vh;
+`;
 
 export default App;
