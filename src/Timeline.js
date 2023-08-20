d4145aaeb0cffffb1c3c3483327e6d0e7edb0d31
E-jiwoo <livei07@naver.com>
2023-08-21 03:01:01 +0900

    FEAT :: 화면에 이미지 추가 - 화면에 메뉴바에 있을 이미지를 넣어주었다

--- src/Timeline.js
+++ src/Timeline.js
@@ -5,6 +5,19 @@ const Timeline = () => {
   return (
     <div className="MyComponent">
       <div className="Profill" />
+
+      <div className="Menu">
+        <img className="Time" alt="Timeline" src="img/Clock Circle.svg" />
+        <br />
+        <img className="Circle" alt="Timeline" src="img/Add Circle.svg" />
+        <br />
+        <img className="Calendar" alt="Timeline" src="img/Calendar.svg" />
+        <br />
+        <img className="Fire" alt="Timeline" src="img/Flame.svg" />
+        <br />
+        <img className="Setting" alt="Timeline" src="img/Settings.svg" />
+      </div>
+      <img className="Alarm" alt="Timeline" src="img/Bell.svg" />
     </div>
   );
 };
